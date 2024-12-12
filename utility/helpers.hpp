4ff9c613d7239c3076c4e73d72e5d7f46fa153bf
RodrigoAroeira <raroeirabr@gmail.com>
2024-12-12 11:51:56 -0300

    feat: Created helpers.hpp for constantly used functions

---
+++ utility/helpers.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+inline std::string getRawText(const std::string &filename) {
+
+  std::ifstream f(filename);
+  if (!f.is_open())
+    throw std::runtime_error("Error opening file");
+
+  std::stringstream buffer;
+  buffer << f.rdbuf();
+  return buffer.str();
+}
+
+inline std::vector<std::string> extractLines(const std::string &text) {
+  std::istringstream iss(text);
+  std::vector<std::string> lines;
+  std::string line;
+  while (std::getline(iss, line))
+    lines.push_back(line);
+
+  return lines;
+  // return {std::istream_iterator<std::string>(iss),
+  //         std::istream_iterator<std::string>()};
+}
