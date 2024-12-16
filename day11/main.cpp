231d2734b1fd4c8ecfa3de3ba2846b6adeee95b4
RodrigoAroeira <raroeirabr@gmail.com>
2024-12-15 23:52:09 -0300

    refactor: Implemented caching

--- day11/main.cpp
+++ day11/main.cpp
@@ -3,61 +3,58 @@
 #include <array>
 #include <cmath>
 #include <iostream>
+#include <map>
 #include <vector>
 
 #include "../utility/helpers.hpp"
 
 using IntType = unsigned long long;
 
-const IntType TOTAL_BLINKS = 25;
-
-void printVector(const std::vector<IntType> &vec) {
-  for (const auto &n : vec)
-    std::cout << n << ' ';
-  std::cout << std::endl;
-}
-
 [[nodiscard]] int getNumberOfDigits(IntType n) {
-  return std::floor(std::log10(n)) + 1;
+  return n == 0 ? 1 : std::floor(std::log10(n)) + 1;
 }
 
-[[nodiscard]] IntType ZeroRule(IntType n) { return 1; }
+IntType stringToInt(const std::string &str) {
+  std::istringstream iss(str);
+  IntType val;
+  iss >> val;
+  return val;
+}
 
 [[nodiscard]] std::array<IntType, 2> EvenDigitsRule(IntType n) {
   std::string asStr = std::to_string(n);
   int divisionPoint = asStr.size() / 2;
 
-  IntType half1 = std::stoull(asStr.substr(0, divisionPoint));
-  IntType half2 = std::stoull(asStr.substr(divisionPoint));
+  IntType half1 = stringToInt(asStr.substr(0, divisionPoint));
+  IntType half2 = stringToInt(asStr.substr(divisionPoint));
 
   return {half1, half2};
 }
 
 [[nodiscard]] IntType ElseRule(IntType n) { return n * 2024; }
 
-void applyRules(std::vector<IntType> &nums) {
+IntType countCached(IntType stone, IntType steps,
+                    std::map<std::pair<IntType, IntType>, IntType> &cache) {
+  auto key = std::make_pair(stone, steps);
+  if (cache.find(key) != cache.end())
+    return cache[key];
 
-  std::vector<IntType> newNums;
+  if (steps == 0)
+    return 1;
+  if (stone == 0)
+    return countCached(1, steps - 1, cache);
 
-  for (int i{}; i < nums.size(); i++) {
-    IntType num = nums[i];
+  IntType res{};
 
-    if (num == 0) {
-
-      newNums.push_back(ZeroRule(num));
-
-    } else if (getNumberOfDigits(num) % 2 == 0) {
-
-      auto [half1, half2] = EvenDigitsRule(num);
-
-      newNums.push_back(half1);
-      newNums.push_back(half2);
-
-    } else {
-      newNums.push_back(ElseRule(num));
-    }
+  if (getNumberOfDigits(stone) % 2 == 0) {
+    auto [half1, half2] = EvenDigitsRule(stone);
+    res = countCached(half1, steps - 1, cache) +
+          countCached(half2, steps - 1, cache);
+  } else {
+    res = countCached(stone * 2024, steps - 1, cache);
   }
-  nums = std::move(newNums);
+
+  return cache[key] = res;
 }
 
 int main() {
@@ -71,12 +68,13 @@ int main() {
       stones.push_back(n);
   }
 
-  std::cout << "Initial stones: ";
-  printVector(stones);
+  std::map<std::pair<IntType, IntType>, IntType> cache;
+  for (IntType blink : {25, 75}) {
+    IntType sum{};
+    for (IntType stone : stones) {
+      sum += countCached(stone, blink, cache);
+    }
 
-  for (int i{1}; i <= TOTAL_BLINKS; i++) {
-    applyRules(stones);
+    std::cout << sum << '\n';
   }
-
-  std::cout << stones.size() << std::endl;
 }
